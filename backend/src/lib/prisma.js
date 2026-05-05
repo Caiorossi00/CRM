3d30af693af076595c7fd9598413c836c5e54a45
Caio Rossi <caio_rossi56@hotmail.com>
2026-05-05 18:33:34 -0300

    chore: remove .env from tracking

--- backend/src/lib/prisma.js
+++ backend/src/lib/prisma.js
@@ -1,11 +1,8 @@
 import 'dotenv/config';
 import { PrismaClient } from '@prisma/client';
-import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
 
-const adapter = new PrismaBetterSqlite3({
-  url: process.env.DATABASE_URL,
-});
+console.log(process.env.DATABASE_URL);
 
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient();
 
 export default prisma;
